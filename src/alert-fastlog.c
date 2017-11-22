a385eb42c13e20e28c1f34ddff56cf33e0a0f8f4
Kris <kkoszyka@gmail.com>
2017-11-22 15:50:42 +0100

    SignatureData - flow_id added

--- src/alert-fastlog.c
+++ src/alert-fastlog.c
@@ -57,19 +57,6 @@
 #include "util-logopenfile.h"
 #include "util-time.h"
 
-//#COLLECTIVE_SENSE #CS_SIGNATURES
-#include <cs/cscommon.h>
-#include <cs/PeriodicMultipleMetricsCollector.h>
-#include "detect-nanomsg.h"
-//#define URL_SIG "ipc:///tmp/signature-pipeline.ipc"
-#define BUF_SIZE_SIG sizeof(SignatureData) * 1
-static __thread NanomsgHandler nn_handler_sig;
-static __thread char nn_init_sig = 0;
-static METRIC_ID fastlog_metric_id = 0;
-
-extern char fast_log_write_to_file;
-//#COLLECTIVE_SENSE_END
-
 #define DEFAULT_LOG_FILENAME "fast.log"
 
 #define MODULE_NAME "AlertFastLog"
@@ -87,6 +74,8 @@ static void AlertFastLogDeInitCtx(OutputCtx *);
 
 int AlertFastLogCondition(ThreadVars *tv, const Packet *p);
 int AlertFastLogger(ThreadVars *tv, void *data, const Packet *p);
+
+#include "cs-alert-fastlog.h"
 
 void AlertFastLogRegister(void)
 {
@@ -199,12 +188,6 @@ int AlertFastLogger(ThreadVars *tv, void *data, const Packet *p)
                 PrintBufferData(alert_buffer, &size, MAX_FASTLOG_ALERT_SIZE, "]\n");
             }
         }
-        //#COLLECTIVE_SENSE #CS_SIGNATURES #TIMEDELTA
-        if( nn_init_sig == 0 )
-        {
-            nn_init_sig = 1;
-            NanomsgInit(&nn_handler_sig, nanomsg_url_sig, BUF_SIZE_SIG);
-        }
 
         /* Write the alert to output file */
         if (unlikely(TRUE == fast_log_write_to_file))
@@ -212,46 +195,8 @@ int AlertFastLogger(ThreadVars *tv, void *data, const Packet *p)
             AlertFastLogOutputAlert(aft, alert_buffer, size);
         }
 
-        SignatureData* sd = (SignatureData*)NanomsgGetNextBufferElement(&nn_handler_sig, sizeof(SignatureData));
-        sd->timestamp_usec = p->ts.tv_sec * 1000000L + p->ts.tv_usec;
-
-        sd->sid = pa->s->id;
-
-        if (pa->s->msg != NULL)
-            strncpy(sd->msg, pa->s->msg, sizeof(sd->msg));
-        else
-            memset(sd->msg, 0, sizeof(sd->msg));
-
-        if (pa->s->class_msg != NULL)
-            strncpy(sd->class_msg, pa->s->class_msg, sizeof(sd->class_msg));
-        else
-            memset(sd->class_msg, 0, sizeof(sd->class_msg));
-
-        sd->priority = pa->s->prio;
-
-        //optionals
-        sd->prot = IP_GET_IPPROTO(p);
-
-        sd->src_ip[0] = 0;
-        sd->src_ip[1] = 0;
-        sd->dst_ip[0] = 0;
-        sd->dst_ip[1] = 0;
-
-        SetIp_NET32_TO_HOST64(GET_IPV4_SRC_ADDR_PTR(p), sd->src_ip);
-        SetIp_NET32_TO_HOST64(GET_IPV4_DST_ADDR_PTR(p), sd->dst_ip);
-
-        if (p->sp > 0)
-            sd->src_port = p->sp;
-        else
-            sd->src_port = 0;
-
-        if (p->dp > 0)
-            sd->dst_port = p->dp;
-        else
-            sd->dst_port = 0;
-
-        NanomsgSendBufferIfNeeded(&nn_handler_sig);
-        update_metric(fastlog_metric_id, 1);
+        //#COLLECTIVE_SENSE #CS_SIGNATURES #TIMEDELTA
+        FillAndSendSIGData(p, pa);
         //#COLLECTIVE_SENSE_END #CS_SIGNATURES_END
     }
 
---
+++ src/cs-alert-fastlog.h
@@ -0,0 +1,66 @@
+#ifndef __CS_ALERT_FASTLOG_H__
+#define __CS_ALERT_FASTLOG_H__
+
+#include <cs/cscommon.h>
+#include <cs/PeriodicMultipleMetricsCollector.h>
+#include "detect-nanomsg.h"
+//#define URL_SIG "ipc:///tmp/signature-pipeline.ipc"
+#define BUF_SIZE_SIG sizeof(SignatureData) * 1
+static __thread NanomsgHandler nn_handler_sig;
+static __thread char nn_init_sig = 0;
+static METRIC_ID fastlog_metric_id = 0;
+
+extern char fast_log_write_to_file;
+
+static void FillAndSendSIGData(const Packet *p, const PacketAlert *pa) {
+    //#COLLECTIVE_SENSE #CS_SIGNATURES #TIMEDELTA
+    if( nn_init_sig == 0 ) {
+        nn_init_sig = 1;
+        NanomsgInit(&nn_handler_sig, nanomsg_url_sig, BUF_SIZE_SIG);
+    }
+
+    SignatureData* sd = (SignatureData*)NanomsgGetNextBufferElement(&nn_handler_sig, sizeof(SignatureData));
+    sd->timestamp_usec = p->ts.tv_sec * 1000000L + p->ts.tv_usec;
+
+    sd->sid = pa->s->id;
+
+    if (pa->s->msg != NULL)
+        strncpy(sd->msg, pa->s->msg, sizeof(sd->msg));
+    else
+        memset(sd->msg, 0, sizeof(sd->msg));
+
+    if (pa->s->class_msg != NULL)
+        strncpy(sd->class_msg, pa->s->class_msg, sizeof(sd->class_msg));
+    else
+        memset(sd->class_msg, 0, sizeof(sd->class_msg));
+
+    sd->priority = pa->s->prio;
+
+    //optionals
+    sd->prot = IP_GET_IPPROTO(p);
+
+    sd->src_ip[0] = 0;
+    sd->src_ip[1] = 0;
+    sd->dst_ip[0] = 0;
+    sd->dst_ip[1] = 0;
+
+    SetIp_NET32_TO_HOST64(GET_IPV4_SRC_ADDR_PTR(p), sd->src_ip);
+    SetIp_NET32_TO_HOST64(GET_IPV4_DST_ADDR_PTR(p), sd->dst_ip);
+
+    if (p->sp > 0)
+        sd->src_port = p->sp;
+    else
+        sd->src_port = 0;
+
+    if (p->dp > 0)
+        sd->dst_port = p->dp;
+    else
+        sd->dst_port = 0;
+
+    sd->flow_id = p->flow->flowInfo.flow_id;
+
+    NanomsgSendBufferIfNeeded(&nn_handler_sig);
+    update_metric(fastlog_metric_id, 1);
+    //#COLLECTIVE_SENSE_END #CS_SIGNATURES_END
+}
+#endif
